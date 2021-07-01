44ee3835777408bcb23639bbda787f8d9af90c5c
Ardian Pantina <ardi@fb.com>
2021-07-01 13:04:32 +0100

    Use config marshalling instead of map

--- integration/create_server.go
+++ integration/create_server.go
@@ -24,24 +24,22 @@ const maxRetries = 10
 
 type ZKServer struct {
 	Version string
-	Config  map[string]string
+	Config  *ServerConfig
 
 	cmd *exec.Cmd
 }
 
 // NewZKServer creates a new ZKServer instance using the given version and config map.
-// Contents of the config map are written to a file which will later be used by the ZK binary.
-func NewZKServer(version string, config map[string]string) (*ZKServer, error) {
+// Contents of the config struct are written to a file which will later be used by the ZK binary.
+func NewZKServer(version string, config *ServerConfig) (*ZKServer, error) {
 	file, err := os.Create(defaultConfigName)
 	if err != nil {
 		return nil, err
 	}
 	defer file.Close()
 
-	for k, v := range config {
-		if _, err = file.WriteString(fmt.Sprintf("%s=%s\n", k, v)); err != nil {
-			return nil, fmt.Errorf("error writing config to file: %v", err)
-		}
+	if err = config.Marshall(file); err != nil {
+		return nil, fmt.Errorf("error writing config to file: %v", err)
 	}
 
 	return &ZKServer{
@@ -69,12 +67,15 @@ func (server *ZKServer) Run() error {
 		log.Printf("successfully downloaded archive %s\n", defaultArchiveName)
 	}
 
-	root, err := extractTarGz(archivePath)
-	if err != nil {
-		return fmt.Errorf("error extracting file: %s\n", err)
+	folderName := "apache-zookeeper-" + server.Version + "-bin"
+	if _, err := os.Stat(filepath.Join(workdir, folderName)); os.IsNotExist(err) {
+		_, err := extractTarGz(archivePath)
+		if err != nil {
+			return fmt.Errorf("error extracting file: %s\n", err)
+		}
 	}
 
-	serverScriptPath := filepath.Join(workdir, root, "bin/zkServer.sh")
+	serverScriptPath := filepath.Join(workdir, folderName, "bin/zkServer.sh")
 	err = os.Chmod(serverScriptPath, 0777)
 	if err != nil {
 		return fmt.Errorf("error changing server script permissions: %s\n", err)
@@ -99,17 +100,6 @@ func (server *ZKServer) Run() error {
 func (server *ZKServer) Shutdown() error {
 	log.Printf("Shutdown() called, killing server process")
 	return server.cmd.Process.Kill()
-}
-
-func DefaultConfig() map[string]string {
-	return map[string]string{
-		"tickTime":               "500",
-		"initLimit":              "10",
-		"syncLimit":              "5",
-		"dataDir":                "/tmp/gozk",
-		"clientPort":             "2181",
-		"4lw.commands.whitelist": "*",
-	}
 }
 
 func downloadToFile(sourceURL, filepath string) error {
---
+++ integration/server_config.go
@@ -0,0 +1,109 @@
+package integration
+
+import (
+	"fmt"
+	"io"
+)
+
+const (
+	DefaultPort                           = 2181
+	DefaultServerTickTime                 = 500
+	DefaultServerInitLimit                = 10
+	DefaultServerSyncLimit                = 5
+	DefaultServerAutoPurgeSnapRetainCount = 3
+	DefaultPeerPort                       = 2888
+	DefaultLeaderElectionPort             = 3888
+)
+
+type ServerConfigServer struct {
+	ID                 int
+	Host               string
+	PeerPort           int
+	LeaderElectionPort int
+}
+
+type ServerConfig struct {
+	TickTime                 int    // Number of milliseconds of each tick
+	InitLimit                int    // Number of ticks that the initial synchronization phase can take
+	SyncLimit                int    // Number of ticks that can pass between sending a request and getting an acknowledgement
+	DataDir                  string // Direcrory where the snapshot is stored
+	ClientPort               int    // Port at which clients will connect
+	AutoPurgeSnapRetainCount int    // Number of snapshots to retain in dataDir
+	AutoPurgePurgeInterval   int    // Purge task internal in hours (0 to disable auto purge)
+	FLWCommandsWhitelist     string
+	Servers                  []ServerConfigServer
+}
+
+type ErrMissingServerConfigField string
+
+func (e ErrMissingServerConfigField) Error() string {
+	return fmt.Sprintf("zk: missing server config field '%s'", string(e))
+}
+
+func DefaultConfig() *ServerConfig {
+	return &ServerConfig{
+		TickTime:             500,
+		InitLimit:            10,
+		SyncLimit:            5,
+		DataDir:              "/tmp/gozk",
+		ClientPort:           2181,
+		FLWCommandsWhitelist: "*",
+		Servers:              nil,
+	}
+}
+
+func (sc ServerConfig) Marshall(w io.Writer) error {
+	// the admin server is not wanted in test cases as it slows the startup process and is
+	// of little unit test value.
+	fmt.Fprintln(w, "admin.enableServer=false")
+	if sc.DataDir == "" {
+		return ErrMissingServerConfigField("dataDir")
+	}
+	fmt.Fprintf(w, "dataDir=%s\n", sc.DataDir)
+	if sc.TickTime <= 0 {
+		sc.TickTime = DefaultServerTickTime
+	}
+	fmt.Fprintf(w, "tickTime=%d\n", sc.TickTime)
+	if sc.InitLimit <= 0 {
+		sc.InitLimit = DefaultServerInitLimit
+	}
+	fmt.Fprintf(w, "initLimit=%d\n", sc.InitLimit)
+	if sc.SyncLimit <= 0 {
+		sc.SyncLimit = DefaultServerSyncLimit
+	}
+	fmt.Fprintf(w, "syncLimit=%d\n", sc.SyncLimit)
+	if sc.ClientPort <= 0 {
+		sc.ClientPort = DefaultPort
+	}
+	fmt.Fprintf(w, "clientPort=%d\n", sc.ClientPort)
+	if sc.AutoPurgePurgeInterval > 0 {
+		if sc.AutoPurgeSnapRetainCount <= 0 {
+			sc.AutoPurgeSnapRetainCount = DefaultServerAutoPurgeSnapRetainCount
+		}
+		fmt.Fprintf(w, "autopurge.snapRetainCount=%d\n", sc.AutoPurgeSnapRetainCount)
+		fmt.Fprintf(w, "autopurge.purgeInterval=%d\n", sc.AutoPurgePurgeInterval)
+	}
+	// enable reconfig.
+	// TODO: allow setting this
+	fmt.Fprintln(w, "reconfigEnabled=true")
+	fmt.Fprintln(w, "4lw.commands.whitelist=*")
+
+	if len(sc.Servers) < 2 {
+		// if we dont have more than 2 servers we just dont specify server list to start in standalone mode
+		// see https://zookeeper.apache.org/doc/current/zookeeperStarted.html#sc_InstallingSingleMode for more details.
+		return nil
+	}
+	// if we then have more than one server force it to be distributed
+	fmt.Fprintln(w, "standaloneEnabled=false")
+
+	for _, srv := range sc.Servers {
+		if srv.PeerPort <= 0 {
+			srv.PeerPort = DefaultPeerPort
+		}
+		if srv.LeaderElectionPort <= 0 {
+			srv.LeaderElectionPort = DefaultLeaderElectionPort
+		}
+		fmt.Fprintf(w, "server.%d=%s:%d:%d\n", srv.ID, srv.Host, srv.PeerPort, srv.LeaderElectionPort)
+	}
+	return nil
+}
