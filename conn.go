2f647ef2485d24ce9cd18d56bf1f965b04d126d0
Ardian Pantina <ardi@fb.com>
2021-07-22 11:56:52 +0100

    Update godoc

--- conn.go
+++ conn.go
@@ -48,7 +48,7 @@ func DialContext(ctx context.Context, network, address string) (*Conn, error) {
 }
 
 // DialContext connects the ZK client to the specified Zookeeper server.
-// The provided context is used to determine the lifetime of the session.
+// The provided context is used to determine the dial lifetime.
 func (client *Client) DialContext(ctx context.Context, network, address string) (*Conn, error) {
 	c := &Conn{
 		passwd: emptyPassword,
