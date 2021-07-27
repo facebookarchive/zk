ac571b35216e773df5818fbc6c3e5ca64a36d63a
Ardian Pantina <ardi@fb.com>
2021-07-27 11:30:30 +0100

    Add CLI flags

--- example/main.go
+++ example/main.go
@@ -2,35 +2,27 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"time"
 
 	"github.com/facebookincubator/zk"
-	"github.com/facebookincubator/zk/integration"
 )
 
 func main() {
-	server, err := integration.NewZKServer("3.6.2", integration.DefaultConfig())
-	if err != nil {
-		fmt.Println("error initializing server:", err)
-		return
-	}
-	if err = server.Run(); err != nil {
-		fmt.Println("error running server:", err)
-		return
-	}
+	path := flag.String("path", "/", "Znode path from which to get data.")
+	address := flag.String("server", "127.0.0.1:2181", "Zookeeper server address.")
 
-	defer server.Shutdown()
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 	client := zk.Client{}
-	conn, err := client.DialContext(ctx, "tcp", "127.0.0.1:2181")
+	conn, err := client.DialContext(ctx, "tcp", *address)
 	if err != nil {
 		fmt.Println("error dialing server:", err)
 		return
 	}
 
-	data, err := conn.GetData("/")
+	data, err := conn.GetData(*path)
 	if err != nil {
 		fmt.Println("getdata error:", err)
 		return
