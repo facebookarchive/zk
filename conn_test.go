b54af9c3630960bfcb06b15baa825ca452d14721
Ardian Pantina <ardi@fb.com>
2021-08-02 11:24:47 +0100

    Use net.Pipe() for mocking net.conn

--- conn_test.go
+++ conn_test.go
@@ -3,6 +3,7 @@ package zk
 import (
 	"context"
 	"errors"
+	"io"
 	"log"
 	"net"
 	"testing"
@@ -66,30 +67,18 @@ func TestGetDataWorks(t *testing.T) {
 }
 
 func TestGetDataNoTimeout(t *testing.T) {
-	cfg := integration.DefaultConfig()
+	sessionCtx, cancelSession := context.WithCancel(context.Background())
+	client, _ := net.Pipe()
 
-	server, err := integration.NewZKServer("3.6.2", cfg)
-	if err != nil {
-		t.Fatalf("unexpected error while initializing zk server: %v", err)
-	}
-	defer server.Shutdown()
-	if err = server.Run(); err != nil {
-		t.Fatalf("unexpected error while calling RunZookeeperServer: %s", err)
-		return
-	}
-	conn, err := DialContext(context.Background(), "tcp", "127.0.0.1:2181")
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-
+	conn := Conn{conn: client, sessionCtx: sessionCtx, cancelSession: cancelSession}
 	// close conn before sending request
 	conn.Close()
-	_, err = conn.GetData("/")
+	_, err := conn.GetData("/")
 	select {
 	case <-time.After(defaultTimeout):
 		t.Fatalf("client should not wait for timeout if connection is closed")
 	default:
-		if err != nil && !errors.Is(errors.Unwrap(err), net.ErrClosed) {
+		if err != nil && !errors.Is(errors.Unwrap(err), io.ErrClosedPipe) {
 			t.Fatalf("unexpected error calling GetData: %v", err)
 		}
 	}
