e2aa402c48410b165d43451d51e27bf9450c33f4
Ardian Pantina <ardi@fb.com>
2021-08-09 16:58:50 +0100

    Refactor example program to use commands separately

--- example/main.go
+++ example/main.go
@@ -10,10 +10,14 @@ import (
 )
 
 func main() {
+	cmd := flag.String("cmd", "get", "Command to be executed. Can be \"get\" or \"list\".")
 	path := flag.String("path", "/", "Znode path from which to get data.")
 	address := flag.String("server", "127.0.0.1:2181", "Zookeeper server address.")
+	flag.Parse()
+
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
+
 	client := zk.Client{}
 	conn, err := client.DialContext(ctx, "tcp", *address)
 	if err != nil {
@@ -21,18 +25,23 @@ func main() {
 		return
 	}
 
-	data, err := conn.GetData(*path)
-	if err != nil {
-		fmt.Println("getData error:", err)
-		return
-	}
-	fmt.Printf("Data for node %s: %v\n", *path, string(data))
+	switch *cmd {
+	case "get":
+		data, err := conn.GetData(*path)
+		if err != nil {
+			fmt.Println("getData error:", err)
+			return
+		}
+		fmt.Printf("Data for node %s: %v\n", *path, string(data))
+	case "list":
+		children, err := conn.GetChildren(*path)
+		if err != nil {
+			fmt.Println("getChildren error:", err)
+			return
+		}
 
-	children, err := conn.GetChildren(*path)
-	if err != nil {
-		fmt.Println("getChildren error:", err)
-		return
+		fmt.Printf("Children of node %s: %v\n", *path, children)
+	default:
+		fmt.Printf("Cannot recognize command \"%s\", exiting.\n", *cmd)
 	}
-
-	fmt.Printf("Children of node %s: %v\n", *path, children)
 }
