187f1945755850b68a1b3ebdc175e0ae69a7a8b3
Ardian Pantina <ardi@fb.com>
2021-08-18 13:32:44 +0100

    Refactor opcodes godoc

--- io/opcodes.go
+++ io/opcodes.go
@@ -1,11 +1,11 @@
 package io
 
-const (
-	// PingXID represents the XID which is used in ping/keepalive packet headers.
-	PingXID = -2
+// PingXID represents the XID which is used in ping/keepalive packet headers.
+const PingXID = -2
 
-	// OpGetData and other constants with the same prefix represent codes used by Zookeeper to differentiate requests.
-	// reference: https://zookeeper.apache.org/doc/r3.4.8/api/constant-values.html#org.apache.zookeeper.ZooDefs.OpCode.getData
+// Below constants represent codes used by Zookeeper to differentiate requests.
+// https://zookeeper.apache.org/doc/r3.4.8/api/constant-values.html#org.apache.zookeeper.ZooDefs.OpCode.getData
+const (
 	OpGetData     = 4
 	OpGetChildren = 8
 	OpPing        = 11
