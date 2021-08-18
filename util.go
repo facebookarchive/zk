ca98df3221c630d103a11cf66e111b82b9925c74
Ardian Pantina <ardi@fb.com>
2021-08-18 15:01:30 +0100

    Remove unused zkConn interface

--- util.go
+++ util.go
@@ -4,13 +4,6 @@ import (
 	"github.com/go-zookeeper/jute/lib/go/jute"
 )
 
-type zkConn interface {
-	GetData(path string) ([]byte, error)
-	GetChildren(path string) ([]string, error)
-	Close() error
-	isAlive() bool
-}
-
 type pendingRequest struct {
 	reply jute.RecordReader
 	done  chan struct{}
