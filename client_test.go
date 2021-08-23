66956f6bb94c45a5eb9c47f35da13acc8d46d34e
Ardian Pantina <ardi@fb.com>
2021-08-23 11:17:09 +0100

    Add custom handler to TestClientRetryLogic to test retries

--- client_test.go
+++ client_test.go
@@ -6,13 +6,28 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/go-zookeeper/jute/lib/go/jute"
+
+	"github.com/facebookincubator/zk/internal/proto"
 	"github.com/facebookincubator/zk/testutils"
 )
 
 const defaultMaxRetries = 5
 
 func TestClientRetryLogic(t *testing.T) {
-	server, err := testutils.NewDefaultServer()
+	failCalls := defaultMaxRetries
+
+	// Create a new handler which will make the test server return an error for a set number of tries.
+	// We expect the client to recover from these errors and retry the RPC calls until success on the last try.
+	server, err := testutils.NewServer(func(header *proto.RequestHeader) jute.RecordWriter {
+		if failCalls > 0 {
+			failCalls--
+			return nil // nil response causes error
+		}
+
+		return testutils.DefaultHandler(header)
+	})
+
 	if err != nil {
 		t.Fatalf("error creating test server: %v", err)
 	}
